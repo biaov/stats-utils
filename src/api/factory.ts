4167db5d5480fdee4c9932386cf79186570ac548
biaov <biaov@qq.com>
2023-09-27 15:41:07 +0800

    update

--- src/api/factory.ts
+++ src/api/factory.ts
@@ -1,8 +1,7 @@
 import type { AxiosRequestConfig } from 'axios'
 import { service } from '@/utils/request'
 
-const factory = service(import.meta.env.VITE_GITHUB_BASE_URL, { Authorization: `bearer ${process.env.GITHUB_TOKEN}` })
-
+const factory = service(import.meta.env.VITE_GITHUB_BASE_URL, { Authorization: `bearer ${import.meta.env.PROD ? process.env.GITHUB_TOKEN : import.meta.env.VITE_TOKEN}` })
 export const command = (path: string) => ({
   get: (query = {}) => factory.get(path, { params: query }) as Promise<Record<string, any>>,
   post: <T>(data = {}, config: AxiosRequestConfig = {}) => factory.post(path, data, config) as Promise<T>
