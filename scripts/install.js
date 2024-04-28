23a9a42b0c89813fcd2e8dbdbe648cb54ad08f42
biaov <biaov@qq.com>
2024-04-28 17:01:05 +0800

    更新代码

--- scripts/install.js
+++ scripts/install.js
@@ -5,4 +5,4 @@ const { dirname } = import.meta
 
 const srcPath = resolve(dirname, '../.env')
 const destPath = resolve(dirname, '../.env.development')
-!existsSync(destPath) && copyFileSync(srcPath, destPath)
+existsSync(srcPath) && !existsSync(destPath) && copyFileSync(srcPath, destPath)
