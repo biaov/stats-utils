0d337577ae7bf575a5d917b3a5a3be6dba294761
biaov <biaov@qq.com>
2024-04-29 09:26:35 +0800

    更新代码

--- dist/index.js
+++ dist/index.js
@@ -117,7 +117,9 @@ const getGithubStats = async (req, res) => {
   }
 };
 const command = factory("https://blog.csdn.net/", {
-  "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"
+  "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
+  "Content-Type": "text/html;charset=utf-8",
+  "Content-Language": "zh-CN"
 });
 const csdnApi = (username) => command(username);
 const getCSDN = async (req, res) => {
