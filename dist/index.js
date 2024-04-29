0558d82427d0aef90e569c9c49af329d5ff2a558
biaov <biaov@qq.com>
2024-04-29 11:24:37 +0800

    更新代码

--- dist/index.js
+++ dist/index.js
@@ -119,12 +119,12 @@ const getGithubStats = async (req, res) => {
 const command = factory("", {
   "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
   "Content-Type": "text/html;charset=utf-8",
-  "Content-Language": "zh-CN"
+  Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
 });
-const csdnApi = (username) => command(`https://biaov.cn/`);
+const csdnApi = (username) => command(`https://blog.csdn.net/${username}`);
 const getCSDN = async (req, res) => {
-  req.params;
-  csdnApi().get().then((data) => {
+  const { username } = req.params;
+  csdnApi(username).get().then((data) => {
     res.json(data);
   }).catch((error) => {
     console.log(error);
--- src/api/card.ts
+++ src/api/card.ts
@@ -3,10 +3,10 @@ import { factory } from './factory'
 const command = factory('', {
   'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
   'Content-Type': 'text/html;charset=utf-8',
-  'Content-Language': 'zh-CN'
+  Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7'
 })
 
 /**
  * csdn
  */
-export const csdnApi = (username: string) => command(`https://biaov.cn/`) // command(`https://blog.csdn.net/${username}`)
+export const csdnApi = (username: string) => command(`https://blog.csdn.net/${username}`) // command(`https://biaov.cn/`) 
